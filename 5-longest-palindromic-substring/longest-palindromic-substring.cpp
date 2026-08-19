4afb275b4cec596dc6be23bd26e07503a9ce33d8
Ashish-Netam <netamashish04@gmail.com>
2026-08-19 23:25:08 +0530

    Time: 4 ms (93.73%) | Memory: 9.2 MB (91.94%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int pallindrome(string& s, int left, int right) {
+
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+    string longestPalindrome(string s) {
+
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int len1 = pallindrome(s, i, i);
+            int len2 = pallindrome(s, i, i + 1);
+
+            int len = max(len1, len2);
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+        return s.substr(start, maxLen);
+    }
+};
